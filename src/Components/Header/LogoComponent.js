a8ad2c5b532111232d8aeeab72675d478cd02ba2
natalijajovanovska <49189513+natalijajovanovska@users.noreply.github.com>
2019-06-28 11:33:18 +0200

    Update LogoComponent.js

--- src/Components/Header/LogoComponent.js
+++ src/Components/Header/LogoComponent.js
@@ -11,7 +11,7 @@ class LogoComponent extends React.Component {
     render() {
         return(
             <div className="col-md-4 col-lg-4 col-xl-4 col-sm-4 col-4 text-center">
-                <button onClick={this.scrollTopFn} className="navbar-brand navbar_brand"><img className=" img-fluid" src={Logo} alt="BRAINSTER" /></button>
+                <button onClick={this.scrollTopFn} className="navbar-brand navbar_brand"><img className="hoverPointer img-fluid" src={Logo} alt="BRAINSTER" /></button>
             </div>
         )
     };
